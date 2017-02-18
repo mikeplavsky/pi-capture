692c4a2bf3e566c98d824983e13c391435906769
mikeplavsky <mike.plavsky@outlook.com>
2017-02-18 18:03:23 +0000

    capturing pictures to s3

---
+++ make.sh
@@ -0,0 +1,10 @@
+DATE=$(date +"%Y-%m-%d_%H_%M")
+BASEDIR=$(dirname "$0")
+
+rm $BASEDIR/*.jpg
+
+FILE=$BASEDIR/$DATE.jpg
+
+fswebcam --fps 15 -S 8 -r 640x480  $FILE
+aws s3 cp $FILE s3://mp-spb-pictures
+
