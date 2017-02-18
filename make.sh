39acb385bdba5d0e6442063e634685a661cf2453
mikeplavsky <mike.plavsky@outlook.com>
2017-02-18 18:07:51 +0000

    making parameter for bucket

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
+BUCKET=$1
+echo $BUCKET 
+
 DATE=$(date +"%Y-%m-%d_%H_%M")
 BASEDIR=$(dirname "$0")
 
@@ -6,5 +9,5 @@ rm $BASEDIR/*.jpg
 FILE=$BASEDIR/$DATE.jpg
 
 fswebcam --fps 15 -S 8 -r 640x480  $FILE
-aws s3 cp $FILE s3://mp-spb-pictures
+aws s3 cp $FILE s3://BUCKET
 
